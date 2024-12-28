43701c84eba4b1786c1d0ca543252245c4d6000a
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2024-12-28 18:14:02 +0530

    promise

---
+++ promise/index.js
@@ -0,0 +1,21 @@
+function register(){
+    console.log("Register end");
+}
+function sendMail(){
+    console.log("Send Mail end");
+}
+function login(){
+    console.log("Login end");
+}
+function getData(){
+    console.log("Get Data end");
+}
+function displayData(){
+    console.log("Display Data end");
+}
+register();
+sendMail();
+login();
+getData();
+displayData();
+console.log("Other applications executed")
